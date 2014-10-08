30f9ef8f98c13d6e499c5575d696bbdd63581327
James Neilson <neilson306@gmail.com>
2014-10-08 18:59:32 -0400

    add unit tests for clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,5 +1,7 @@
 class Club < ActiveRecord::Base
 
+	validates :name, presence: true, uniqueness: true
+
 	has_many :bookings
 
 end
--- test/models/club_test.rb
+++ test/models/club_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ClubTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "a club must have a name" do
+		assert_difference('Club.count', 0) do
+			Club.create()
+		end
+	end
+
+	test "a club must have a unique name" do
+		assert_difference('Club.count', 0) do
+			Club.create(name: clubs(:grassy_acres).name)
+		end
+	end
+
 end
