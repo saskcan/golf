8892d0f9b74682f4b83e3ad826ea716aa0874d5a
James Neilson <neilson306@gmail.com>
2014-10-08 13:19:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GolfClub::Application.config.secret_key_base = '01263a578887f5fd6dc4c9d055a1ae3d671a01f8f8838138760a0a8c43c912af9553679dde27f15849ed9dd8ef46c0a434ad7d267cd8a1c73901e6394ccff13b'
