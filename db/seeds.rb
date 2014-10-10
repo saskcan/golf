1ddba8dcf04b13ebad54ae51213746532f7d1448
James Neilson <neilson306@gmail.com>
2014-10-09 20:35:02 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+rattlesnake = Club.create(name: 'Rattlesnake Pit Golf Course')
+green = Club.create(name: 'Green Acres')
+happy = Club.create(name: "Happy Ernie's Driving Range")
+
+amy = User.create(email: 'amy@coupgon.com', password: 'secret123')
+
+Booking.create(user: amy, club: rattlesnake, time: "2014-12-25 9:00:00")
+Booking.create(user: amy, club: green, time: "2014-01-01 12:40:00")
